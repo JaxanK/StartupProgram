3aceac6490131ef7b62ee950cc05522c79c02f01
JaxanK <jkay@scottspringfield.com>
2023-08-02 13:48:24 -0600

    Changing Example Bat file to make it a little clearer

--- Example bat file that Will get run by program.bat
+++ Example bat file that Will get run by program.bat
@@ -1,2 +1,3 @@
-cscript ExampleScript(OnlyCalledFromBatFile).vbs "Program is started. Press ok to Have splash screen end"
+cscript ExampleScript(OnlyCalledFromBatFile).vbs "Program is started. Notice the Splash screen. You can hit cancel and this script will end abruptly. Or press OK to have splash screen end and see the next message in this script."
 echo ProgramLaunchComplete
+cscript ExampleScript(OnlyCalledFromBatFile).vbs "Notice Startup Program has ended but this script is still running. This means any programs started with this script will continue to run. Press OK to end this example."
